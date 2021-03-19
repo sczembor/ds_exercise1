55305738fef280f18c2d73895a8c6706260edc95
Jan Ferbr <honza.ferbr@gmail.com>
2021-03-19 11:47:25 +0100

    tried the concurrency

--- client.c
+++ client.c
@@ -63,8 +63,10 @@ int main (int argc, char **argv)
         n = readLine(0, buffer, MAX_MSG_SIZE);
         strcpy(mes1.val1,buffer);
         printf("Please insert  value2:  (1 integer confirm with /0 \n");
+        n = readLine(0, buffer, MAX_MSG_SIZE);
         strcpy(mes1.val2,buffer);
         printf("Please insert  the key:  (1 float confirm with /0 \n");
+        n = readLine(0, buffer, MAX_MSG_SIZE);
         strcpy(mes1.val3,buffer);
         if (n!=-1){
             int msg;
