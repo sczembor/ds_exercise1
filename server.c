62499a0c4d281971282696c159ef1673b2221029
Jan Ferbr <honza.ferbr@gmail.com>
2021-03-20 13:38:19 +0100

    fuck off

--- server.c
+++ server.c
@@ -103,7 +103,7 @@ int main(int argc, char **arv)
     while(1){
         printf("creating  thread\n");
         pthread_create(&thread,&thread_attr,manage_request,&qd_server); //HERE!!!!!
-        //pthread_cond_wait(&mutex1,&signal1);
+        pthread_cond_wait(&mutex1,&signal1);
         pthread_mutex_lock(&mutex1);
         while(busy==TRUE){
             pthread_cond_wait(&mutex1,&signal1);
