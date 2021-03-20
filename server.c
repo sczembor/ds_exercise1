e49dec34e798e661dfaef3acfbf5fe11cbacadf9
Jan Ferbr <honza.ferbr@gmail.com>
2021-03-20 16:18:59 +0100

    fuck off

--- server.c
+++ server.c
@@ -69,9 +69,9 @@ void manage_request (mqd_t *s) {
         perror ("Server: mq_receive");
         exit (1);
     }
-    pthread_cond_signal(&signal1);
+    //pthread_cond_signal(&signal1);
     pthread_mutex_unlock(&mutex1);
-    busy = FALSE;
+    //busy = FALSE;
     printf ("Server: message received: %s,%s,%i,%f\n",&in_buffer.key, &in_buffer.value1, in_buffer.value2, in_buffer.value3);
     i--;
     printf("number of running threads is %i\n exiting thread!\n",i);
@@ -106,24 +106,25 @@ int main(int argc, char **arv)
         exit (1);
     }
 
-    while(i<10){
+    while(1){
         if (mq_getattr(qd_server, &attr) == -1)
             perror("mq_getattr");
         
         //printf("number of messages in queue is %i\n",attr.mq_curmsgs);
         //wait(1000);
         
-        if (attr.mq_curmsgs>0){
-            printf("creating  thread because buffer not empty\n");
+        if (attr.mq_curmsgs>0 && i<10){
+            printf("number of messages in queue is %i\n",attr.mq_curmsgs);
+            printf("creating thread because buffer not empty\n");
             pthread_create(&thread[i],&thread_attr,manage_request,&qd_server);
             i++;
             //pthread_mutex_lock(&mutex1);
-            printf("mutex1 locked in main\n");
-            while(busy==TRUE){
-                pthread_cond_wait(&mutex1,&signal1);
-            }
+            //printf("mutex1 locked in main\n");
+            //while(busy==TRUE){
+            //    pthread_cond_wait(&mutex1,&signal1);
+            //}
             //pthread_mutex_unlock(&mutex1);
-            busy=TRUE;
+            //busy=TRUE;
         }
     }
     return 0;
