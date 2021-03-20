e849a154e190abe9c80326765a3c6e433e6b9973
Jan Ferbr <honza.ferbr@gmail.com>
2021-03-20 14:42:17 +0100

    fuck off

--- server.c
+++ server.c
@@ -116,14 +116,16 @@ int main(int argc, char **arv)
             perror("mq_notify");
         
         //pthread_create(&thread,&thread_attr,manage_request,&qd_server); //HERE!!!!!
-        //pthread_cond_wait(&mutex2,&signal1);
         
+        pthread_cond_wait(&mutex2,&signal1);
+        /*
         pthread_mutex_lock(&mutex1);
         printf("mutex1 locked in main\n");
         while(busy==TRUE){
             pthread_cond_wait(&mutex1,&signal1);
         }
         pthread_mutex_unlock(&mutex1);
+        */
         busy=TRUE;
     }
     
