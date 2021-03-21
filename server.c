d2a0af71fd00a31bbc1a26553f4b14fac4f9c876
Jan Ferbr <honza.ferbr@gmail.com>
2021-03-21 15:03:01 +0100

    fuck off

--- server.c
+++ server.c
@@ -107,7 +107,6 @@ int main(int argc, char **arv)
         perror ("Server: mq_open (server)");
         exit (1);
     }
-    busy=FALSE;
     while(1){
         if (mq_getattr(qd_server, &attr) == -1)
             perror("mq_getattr");
