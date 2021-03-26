f9cf3f739e2a5c5940f46e810c0c5de7e433baea
sczembor <43810037+sczembor@users.noreply.github.com>
2021-03-26 14:14:55 +0100

    minor fixes

--- server.c
+++ server.c
@@ -211,7 +211,7 @@ int searchList(char* key)
     while(tmp != NULL)
     {
         printf("im in searchlist while loop\n");
-        printf("wartość key:%s\n", key);
+        //printf("wartość key:%s\n", key);
         printf("wartość tmp->:%s\n",tmp->key);
         printf("im in searchlist while loop\n");
         int i = strcmp(key, tmp->key);
