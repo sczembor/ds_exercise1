d93283867ed4fcbcafc2a401a7ee28f2be241b37
sczembor <43810037+sczembor@users.noreply.github.com>
2021-03-26 14:29:42 +0100

    minor fixes

--- server.c
+++ server.c
@@ -71,7 +71,7 @@ void manage_request (mqd_t *s) {
     if(in_buffer.type == 1){
         in_buffer.type = deleteList();
     }else if(in_buffer.type == 2){
-        if(searchList(&in_buffer.key)==1){
+        if(searchList(&in_buffer.key)==0){
             printf("im am in if statement for 2");
             in_buffer.type = addNode(&in_buffer.key,&in_buffer.value1,&in_buffer.value2,&in_buffer.value3);
             printf("after invoke of addNode");
