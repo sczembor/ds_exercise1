63d1aa5e234e60555e1e7bccc5d0010921c1fe71
sczembor <43810037+sczembor@users.noreply.github.com>
2021-03-28 20:12:17 +0200

    minor fixes

--- client.c
+++ client.c
@@ -82,6 +82,7 @@ int main (int argc, char **argv)
             case 3://get_value
                 check=get_value(qd_server,qd_client,mes1,attr);
                 printf("function returned:%i\n",check);
+                printf("key:%s\nvalue1:%s,value2:%d\nvalue3:%f\n", mes1.key,mes1.val1,mes1.val2,mes1.val3);
                 break;
             case 4://modify value
                 check=modify_value(qd_server,qd_client,mes1,attr);
